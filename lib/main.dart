28361df77e1ee8141125c12424f37bc7bf6f7d55
rifkyabdulhanan <rifkyabdulhanan@gmail.com>
2025-10-13 02:56:36 +0000

    Splash & Logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart'; // 💡 Tambahkan ini
 import 'package:montugo/Mechanic/UI/NavigationBar/custom_bottom_navbar.dart';
 import 'package:montugo/Screens/Catalog/category_catalog.dart';
 import 'package:montugo/Screens/FaQ/faq.dart';
@@ -7,9 +8,11 @@ import 'package:montugo/Screens/FaQ/faq.dart';
 import 'package:montugo/Screens/home.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding); 
   await Firebase.initializeApp();
   runApp(const MyApp());
+  FlutterNativeSplash.remove(); 
 }
 
 class MyApp extends StatefulWidget {
@@ -40,12 +43,12 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          body: _pages[_selectedIndex],
-          backgroundColor: Colors.white,
-          bottomNavigationBar: CustomBottomNavbar(
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-          )),
+        body: _pages[_selectedIndex],
+        backgroundColor: Colors.white,
+        bottomNavigationBar: CustomBottomNavbar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        )),
     );
   }
 }
